56c840752dc3b04d44aaa3aeaeba83d5085e8486
devkeydet <devkeydet@users.noreply.github.com>
2024-03-06 18:22:09 -0500

    cleanup

--- codefirstsolution/pack-with-mappings.ps1
+++
@@ -1,2 +0,0 @@
-Set-PSDebug -Trace 1
-pac solution pack --folder .\src\ --zipfile .\bin\Debug\temp-packed-with-mappings.zip --map .\map.xml --packagetype both
--- codefirstsolution/unpack-with-mapping.ps1
+++
@@ -1,2 +0,0 @@
-Set-PSDebug -Trace 1
-pac solution unpack --folder .\src\ --zipfile codefirstsolution.zip --map .\map.xml --packagetype both --processCanvasApps
