48ab56b0d52a57d335e38324358e5aa8750afdf7
devkeydet <devkeydet@users.noreply.github.com>
2024-03-06 16:03:09 -0500

    working again

---
+++ codefirstsolution/pack-with-mappings.ps1
@@ -0,0 +1,2 @@
+Set-PSDebug -Trace 1
+pac solution pack --folder .\src\ --zipfile .\bin\Debug\temp-packed-with-mappings.zip --map .\map.xml --packagetype both
---
+++ codefirstsolution/unpack-with-mapping.ps1
@@ -0,0 +1,2 @@
+Set-PSDebug -Trace 1
+pac solution unpack --folder .\src\ --zipfile codefirstsolution.zip --map .\map.xml --packagetype both --processCanvasApps
