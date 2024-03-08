eff36f193785e882c24d1a0b44a070bcec94a242
devkeydet <devkeydet@users.noreply.github.com>
2024-03-07 19:38:18 -0500

    tweaks

--- cleanup-minify.ps1
+++ cleanup-minify.ps1
@@ -1,4 +1,6 @@
-Write-Host "Cleaning up web resources minification..."
-
-Remove-Item -Path webresource-src -Recurse
-Rename-Item -Path webresource-src-holding -NewName webresource-src
+Write-Host "Resetting web resources minification..."
+$webResourceSrcHolding = "obj\release\webresource-src"
+$webResourceSrc = "webresource-src"
+Remove-Item -Path $webResourceSrc -Recurse
+New-Item -ItemType directory -Path $webResourceSrc
+Copy-Item -Path "$webResourceSrcHolding\*" -Destination $webResourceSrc -Force
--- minify.ps1
+++ minify.ps1
@@ -1,13 +1,13 @@
 Write-Host "Minifying web resources..."
 
 $webResourceSrc = "webresource-src"
-$webResourceSrcHolding = "webresource-src-holding"
+$webResourceSrcHolding = "obj\release\webresource-src"
 
 if (!(Test-Path -Path $webResourceSrcHolding)) {
     New-Item -ItemType directory -Path $webResourceSrcHolding
 }
 
-Copy-Item -Path "$webResourceSrc\*" -Destination $webResourceSrcHolding
+Copy-Item -Path "$webResourceSrc\*" -Destination $webResourceSrcHolding -Force
 $files = Get-ChildItem -Path $webResourceSrc -Filter *.js
 
 foreach ($file in $files) {
