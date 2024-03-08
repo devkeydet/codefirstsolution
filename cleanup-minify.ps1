05cd6bca9d368f9da65d5c53a8ed5f9c8088f9ca
devkeydet <devkeydet@users.noreply.github.com>
2024-03-07 19:47:52 -0500

    simplified minification

--- cleanup-minify.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Resetting web resources minification..."
-$webResourceSrcHolding = "obj\release\webresource-src"
-$webResourceSrc = "webresource-src"
-Remove-Item -Path $webResourceSrc -Recurse
-New-Item -ItemType directory -Path $webResourceSrc
-Copy-Item -Path "$webResourceSrcHolding\*" -Destination $webResourceSrc -Force
--- minify.ps1
+++ minify.ps1
@@ -1,14 +1,16 @@
 Write-Host "Minifying web resources..."
 
 $webResourceSrc = "webresource-src"
-$webResourceSrcHolding = "obj\release\webresource-src"
+$webResourceSrcMinifyTemp = "obj\release\webresource-src"
 
-if (!(Test-Path -Path $webResourceSrcHolding)) {
-    New-Item -ItemType directory -Path $webResourceSrcHolding
+if (!(Test-Path -Path $webResourceSrcMinifyTemp)) {
+    New-Item -ItemType directory -Path $webResourceSrcMinifyTemp
+} else {
+    Get-ChildItem $webResourceSrcMinifyTemp | Remove-Item -Recurse -Force
 }
 
-Copy-Item -Path "$webResourceSrc\*" -Destination $webResourceSrcHolding -Force
-$files = Get-ChildItem -Path $webResourceSrc -Filter *.js
+Copy-Item -Path "$webResourceSrc\*" -Destination $webResourceSrcMinifyTemp -Force
+$files = Get-ChildItem -Path $webResourceSrcMinifyTemp -Filter *.js
 
 foreach ($file in $files) {
     uglifyjs $file.FullName -o $file.FullName
